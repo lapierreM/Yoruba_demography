c3a99283dee1b1361b6a4216671e3930f944f6be
Marguerite Lapierre <marguerite.lapierre@mnhn.fr>
2016-09-28 15:21:18 +0200

    updated Programs/plot/plot_demographies_parameters.py with new file path

--- Programs/plot/plot_demographies_parameters.py
+++ Programs/plot/plot_demographies_parameters.py
@@ -11,7 +11,7 @@ Parameter file for the plot_demographies.py program
 models_file="../Output_files/FoldSFS_YRI.txt.models"
 
 # String, path to the file where the Birth-Death trajectory is
-BD_trajectory_file="/Users/margueritelapierre/Desktop/BD_traj_5000rep_diff1.txt"
+BD_trajectory_file="../Output_files/example_BD_trajectory.txt"
 
 # String, path to the file where the Conditioned trajectory is
 Cond_trajectory_file="../Output_files/example_Conditioned_trajectory.txt"
